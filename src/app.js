e49c556e7f0e4030adbd86f8f7092bbac3fc90d5
Faith Mofokeng <faithmofokeng5@gmail.com>
2021-11-09 08:05:21 +0200

    added port

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const pool = require('./config')
+const pool = require("./config");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,81 +8,90 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-pool.connect((_, err) => {
-  if(err){
-    throw new Error('Not connected')
+pool.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("Server is running");
   }
-  console.log('Connected')
 });
 
-app.get("/", () => {
-  res.send("Connected"),
-  (_, err) => {
-    if(err){
-        throw new Error('Not connected')
-    }}
+app.get("/", (err, data) => {
+  if (err) {
+    throw err;
+  } else {
+    data.send("Connected");
+  }
 });
 const createTable = () => {
   pool.query(
     "CREATE TABLE Visitors ( id SERIAL PRIMARY KEY, name VARCHAR(50), age INT, date  DATE, time TIME, assistor VARCHAR(50), comments VARCHAR(100))",
-    (_, err) => {
-      if(err){
-          throw new Error('Operation unsuccessful, table was not created')
+    (err, data) => {
+      if (err) {
+        throw error("Operation unsuccessful, table was not created");
       }
-      console.table(res.rows[0])
-  })
-}
+      console.log(data.rows[0]);
+    }
+  );
+};
 
 app.get("/viewVisitors", () => {
   createTable();
-  pool.query("SELECT DISTINCT ID, Name FROM Visitors"),(_, err) => {
-    if(err){
-        throw new Error('Operation unsuccessful, cannot view visitors')
-    }
-    console.table(res.rows)
-    console.log(res.rows)
-
-    }
-})
+  pool.query("SELECT DISTINCT ID, Name FROM Visitors"),
+    (err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log(data.rows);
+      }
+    };
+});
 
 app.get("/viewVisitor:id", () => {
-  pool.query("SELECT * FROM Visitors WHERE id = ${id}"), (_, err) => {
-    if(err){
-        throw new Error('Operation unsuccessful, cannot view visitors')
-    }
-    console.table(res.rows);
-}
+  pool.query("SELECT * FROM Visitors WHERE id = ${id}"),
+    (err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log(data.rows);
+      }
+    };
 });
 
 app.post("/addNewVisitor", () => {
   pool.query(
     `INSERT INTO Visitors(name, age, date, time, assistor, comments) VALUES ($1, $2, $3, $4, $5, $6)`,
     [data.name, data.age, data.date, data.time, data.assistor, data.comments],
-    (_, err) => {
-      if(err){
-          throw new Error('Operation unsuccessful, cannot insert visitors')
+    (err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log(data.rows);
       }
-      console.table(res.rows);
     }
-  )
-})
+  );
+});
 
 app.delete("/deleteVisitor:id", () => {
-  pool.query(`DELETE FROM Visitors WHERE id = ${id}`), (_, err) => {
-    if(err){
-        throw new Error('Operation unsuccessful, cannot delete visitor')
-    }
-    console.table(`Deleted: ${res.rows}`);
-}
-})
+  pool.query(`DELETE FROM Visitors WHERE id = ${id}`),
+    (err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log(`Deleted: ${data.rows}`);
+      }
+    };
+});
 
 app.delete("/deleteAllVisitors", () => {
-  pool.query(`DELETE FROM Visitors`), (_, err) => {
-    if(err){
-        throw new Error('Operation unsuccessful, cannot delete visitors')
-    }
-    console.table(`Deleted: ${res.rows}`);
-}
+  pool.query(`DELETE FROM Visitors`),
+    (err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log(`Deleted: ${data.rows}`);
+      }
+    };
 });
 
 app.put("/updateVisitor:id", () => {
@@ -96,15 +105,17 @@ app.put("/updateVisitor:id", () => {
       data.assistant,
       data.comments,
       data.id,
-    ], (_, err) => {
-      if(err){
-          throw new Error('Operation unsuccessful, cannot update visitors')
+    ],
+    (err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log(`Data updated ${data.rowCount}`);
       }
-      console.log('Data updated', res.rowCount);
-  }
-  )
+    }
+  );
 });
 
 app.listen(port, () => {
-  console.log("server is running");
+  console.log(`server is running on ${port}`);
 });
